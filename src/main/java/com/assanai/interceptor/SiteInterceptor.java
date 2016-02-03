cd967369d2188119794b82ec50f7445fa4490fe3
amanurat <assanai17@gmail.com>
2016-02-03 16:13:34 +0700

    cleanup interceptor to remove locales

--- src/main/java/com/assanai/interceptor/SiteInterceptor.java
+++ src/main/java/com/assanai/interceptor/SiteInterceptor.java
@@ -1,43 +1,26 @@
 package com.assanai.interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Locale;
 
 @Controller
 public class SiteInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private MessageSource messageSource;
-
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("SiteInterceptor preHandle");
         return true;
     }
 
-    public void postHandle(HttpServletRequest request,
-                           HttpServletResponse response, Object handler,
-                           ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("SiteInterceptor postHandle");
-
-
     }
 
-    public void afterCompletion(HttpServletRequest request,
-                                HttpServletResponse response, Object handler, Exception exception)
-            throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
         System.out.println("SiteInterceptor afterCompletion");
-        System.out.println("site.maintenance: " + messageSource.getMessage("site.maintenance", null, Locale.ENGLISH));
-
-
-        System.out.println("site.maintenance: " + messageSource.getMessage("test.param", null, Locale.ENGLISH));
     }
 
 
---
+++ src/main/java/com/assanai/jdbc/AccountJdbc.java
@@ -0,0 +1,11 @@
+package com.assanai.jdbc;
+
+/**
+ * Traditional connecting with JDBC
+ * Created by amanurat on 2/2/2016 AD.
+ */
+public class AccountJdbc {
+
+
+
+}
